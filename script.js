f5fafb555a93fd482e970fece81c69d4faba8a14
Marcelo Kirino <mhkirino@hotmail.com>
2022-11-21 19:53:42 -0300

    Primeiro envio do código de sliders
    O código está com problema,
    Tem 3 imagens e por algum motivo a terceira imagem está sendo ignorada, e quando clico em certas vezes só vai até a metade da próxima imagem.

---
+++ script.js
@@ -0,0 +1,26 @@
+let totalSlides = document.querySelectorAll('.slider_item').length;
+let sliderWidth = document.querySelector('.slider_width');
+let slideCount = 0;
+
+function previous() {
+    slideCount--;
+    if (slideCount < 0) {
+        slideCount = totalSlides - 1;
+    }
+    updateMargin()
+}
+
+function next() {
+    slideCount++;
+    if (slideCount > (totalSlides - 1)) {
+        slideCount = 0;
+    }
+    updateMargin()
+}
+
+function updateMargin() {
+    let imageWidth = document.querySelector('.slider_item').clientWidth;
+    let newMargin = (slideCount * imageWidth);
+    sliderWidth.style.marginLeft = `-${newMargin}px`;
+}
+
